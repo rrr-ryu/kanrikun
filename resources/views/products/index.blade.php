9fb57660fea557a4768bd072ac5dabd951d3b78c
rrr-ryu <zero.requiem910219@gmail.com>
2023-03-24 23:45:41 +0900

    修正

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,4 +1,6 @@
+@extends('layouts.app')
 
+@section('content')
 <div class="container">
   商品情報一覧画面
     <div class="row">
@@ -32,32 +34,32 @@
                 @if ($products->count() === 0)
                     <p>検索結果が見つかりませんでした</p>
                 @else
-                @foreach ( $products as $product )
-                <tr>
-                    <td>{{ $product->id }}</td>
-                    <td>
-                        @if ($product->img_path)
-                            <img src="{{ asset('storage/product/' . $product->img_path) }}" class="img-thumbnail" alt="..."></td>
-                        @else
-                            <p>画像なし</p>
-                        @endif
-                    <td>{{ $product->product_name }}</td>
-                    <td>{{ $product->price }}</td>
-                    <td>{{ $product->stock }}</td>
-                    <td>{{ $product->company->company_name }}</td>
-                    <td><button class="btn btn-success" onclick="location.href='{{ route('products.show',['product' => $product->id]) }}'">詳細</button></td>
-                    <td>
-                        <form id="delete_{{$product->id}}" method="post" action="{{ route('products.destroy', ['product' => $product->id])}}">
-                            @csrf
-                            @method('delete')
-                            <a class="btn btn-danger" href="#" data-id="{{ $product->id }}" onclick="deletePost(this)">削除</a>
-                        </form>
-                    </td>
-                  </tr>
-                  @endforeach
+                    @foreach ( $products as $product )
+                    <tr>
+                        <td>{{ $product->id }}</td>
+                        <td>
+                            @if ($product->img_path)
+                                <img src="{{ asset('storage/product/' . $product->img_path) }}" class="img-thumbnail" alt="..."></td>
+                            @else
+                                <p>画像なし</p>
+                            @endif
+                        <td>{{ $product->product_name }}</td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->stock }}</td>
+                        <td>{{ $product->company->company_name }}</td>
+                        <td><button class="btn btn-success" onclick="location.href='{{ route('products.show',['product' => $product->id]) }}'">詳細</button></td>
+                        <td>
+                            <form id="delete_{{$product->id}}" method="post" action="{{ route('products.destroy', ['product' => $product->id])}}">
+                                @csrf
+                                @method('delete')
+                                <a class="btn btn-danger" href="#" data-id="{{ $product->id }}" onclick="deletePost(this)">削除</a>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
                 @endif
-              </tbody>
-          </table>
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
